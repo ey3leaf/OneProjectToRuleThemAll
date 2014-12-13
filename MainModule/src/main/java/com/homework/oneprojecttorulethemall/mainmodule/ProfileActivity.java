8dc5c19df9068601fd73ddc82d8b963ec05683c3
ey3leaf <ey3leaf@gmail.com>
2014-12-13 16:11:09 +0200

    Added ProfileActivity Included pre-Drawer Added logout dialog Login, Logout, Map exists

--- MainModule/src/main/java/com/homework/oneprojecttorulethemall/mainmodule/ProfileActivity.java
+++ MainModule/src/main/java/com/homework/oneprojecttorulethemall/mainmodule/ProfileActivity.java
@@ -1,39 +1,44 @@
 package com.homework.oneprojecttorulethemall.mainmodule;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 
-public class ProfileActivity extends ActionBarActivity {
+public class ProfileActivity extends Activity {
+
+    private ImageView avatar;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        avatar = (ImageView) findViewById(R.id.avatar);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
-        return true;
+    public void camera(View v) {
+        Intent takeAvatarIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takeAvatarIntent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(takeAvatarIntent, 1);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            avatar.setImageBitmap(bitmap);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void done(View v) {
+
     }
 }
